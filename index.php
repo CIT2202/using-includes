9c87fc37359117873e8516520d22d1326f97670b
mmantle-hud <m.e.mantle@hud.ac.uk>
2016-12-06 11:14:56 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>All About Huddersfield</title>
+</head>
+<body>
+<header>
+<div> All About Huddersfield</div>
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="sport.php">Sport</a></li>
+        <li><a href="industry.php">Industry</a></li>
+    </ul>
+</nav>
+</header>
+
+<section>
+<h1>Huddersfield</h1>
+<p>
+Huddersfield is a large market town and is the largest settlement in the metropolitan borough of Kirklees, West Yorkshire, England. It is the 11th largest town in the United Kingdom with a population of 162,949 (2011 census).Halfway between Leeds and Manchester, it lies 190 miles (310 km) north of London, and 10.3 miles (16.6 km) south of Bradford, the nearest city.</p>
+<p>Huddersfield is a town known for sport, home to the rugby league team, Huddersfield Giants, founded in 1895, who play in the European Super League and Football League Championship football team Huddersfield Town F.C., founded in 1908. The town is home to the University of Huddersfield and the sixth form colleges Greenhead College, Kirklees College and Huddersfield New College.</p>
+<section>
+
+<footer>
+Text modified from 
+<a href="https://en.wikipedia.org/wiki/Huddersfield">
+https://en.wikipedia.org/wiki/Huddersfield
+</a>
+</footer>
+</body>
+</html>
---
+++ sport.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>All About Huddersfield</title>
+</head>
+<body>
+<header>
+<div> All About Huddersfield</div>
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="sport.php">Sport</a></li>
+        <li><a href="industry.php">Industry</a></li>
+    </ul>
+</nav>
+</header>
+
+<section>
+<h1>Sport in Huddersfield</h1>
+<p>Football and rugby league are the main spectator sports in Huddersfield.
+ Its professional football team, Huddersfield Town F.C. play in the Championship. In 1926, the club became the first in England to win three successive league titles, a feat only three other clubs have matched.The town was the birthplace of rugby league, and is home to the Huddersfield Giants who play in the Super League, the top division in Europe. The town is home to Huddersfield Underbank Rangers rugby league club, who play in the Rugby League Conference. Huddersfield Rugby Union Football Club play in the National Division Three North and Huddersfield Rams Aussie Rules club. The main sporting arena is John Smith's Stadium home to both the football team and rugby league side.
+ </p>
+<section>
+
+<footer>
+Text modified from 
+<a href="https://en.wikipedia.org/wiki/Huddersfield">
+https://en.wikipedia.org/wiki/Huddersfield
+</a>
+</footer>
+</body>
+</html>
