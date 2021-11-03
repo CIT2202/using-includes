e53a53430804afba9e2af722543fff66133da63a
mmantle-hud <m.e.mantle@hud.ac.uk>
2021-11-03 14:16:44 +0000

    Update industry.php

--- hud-guide/industry.php
+++ hud-guide/industry.php
@@ -1,46 +1,16 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-<head>
-	<title>All About Huddersfield</title>
-	<meta http-equiv="content-type" content="text/html;charset=utf-8" >
-</head>
-<body>
-<header>
-<div> All About Huddersfield</div>
-<nav>
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="sport.php">Sport</a></li>
-        <li><a href="industry.php">Industry</a></li>
-    </ul>
-</nav>
-</header>
-
-=======
 <?php
 $pageTitle="Industry";
 include "header.php";
 ?>
->>>>>>> 78a33fe88f1b815f0f3e8c994ffc072056c5ba2b
+
 <section>
 <h1>Huddersfield Industry</h1>
 <p>
 Huddersfield is a manufacturing town, despite the university being the largest employer. Historically the town produced textiles. The number of people who work in textiles has declined, but the surviving companies produce large quantities of woollen products with little labour. The town is home to textile, chemical and engineering companies; including Brook Motors Ltd founded by Ernest Brook in 1904,that would become known all over the world. Against conventional wisdom, he started making alternating current electric motors, and he did this in one room with two assistants and starting capital of just £300. On its 50th anniversary in 1954 it employed more than 2,000 people and, with Ernest’s sons Frank and Jack in charge, was the largest exclusive producer of AC motors in the world, and had a turnover of £4,500,000. That same year Brook Motors Ltd operated 10 factories in Huddersfield, its biggest being Empress Works on St Thomas’s Road, and had just opened one at Barugh Green, Barnsley.Cummins Turbo Technologies (turbocharger manufacturers), Huddersfield Fine Worsteds (textiles), Taylor & Lodge (textiles), C & J Antich (textiles), Syngenta AG (agro-chemicals), Pennine Radio Limited (electronics transformers and sheet metalworking) and a large number of niche manufacturers, such as Dual Seal Glass (maker of spandrel glass panels) and Ellis Furniture (producer of kitchen and bathroom furniture). Huddersfield is home to Andrew Jones Pies a regional award winning pie-maker, where a worker was killed in a gas explosion on 10 April 2009 and Mamas and Papas – a manufacturer and retailer of prams, pushchairs and related items.
  </p>
 <section>
-<<<<<<< HEAD
 
-<footer>
-Text modified from
-<a href="https://en.wikipedia.org/wiki/Huddersfield">
-https://en.wikipedia.org/wiki/Huddersfield
-</a>
-</footer>
-</body>
-</html>
-=======
 <?php
 include "footer.php";
 ?>
->>>>>>> 78a33fe88f1b815f0f3e8c994ffc072056c5ba2b
+
