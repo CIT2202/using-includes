5b4a2e360fa28eaa53167364ac61d5ae0ce180d3
mmantle-hud <m.e.mantle@hud.ac.uk>
2021-11-03 14:17:33 +0000

    Update university.php

--- hud-guide/university.php
+++ hud-guide/university.php
@@ -2,10 +2,12 @@
 $pageTitle="University";
 include "header.php";
 ?>
+
 <section>
 <h1>Huddersfield University</h1>
 <p>The University of Huddersfield (informally Huddersfield University) is a public university located in Huddersfield, West Yorkshire, England. It has been a University since 1992, but has its origins in a series of institutions dating back to the 19th century. It has made teaching quality a particular focus of its activities, winning the inaugural Higher Education Academy Global Teaching Excellence Award, and achieving a TEF Gold Award, both in 2017.</p>
 <section>
+  
 <?php
 include "footer.php";
 ?>
