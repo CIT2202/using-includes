55a63b4ca8ee4e15bcedc6303df7b2aaaaf5a9dd
mmantle-hud <m.e.mantle@hud.ac.uk>
2021-11-03 14:15:34 +0000

    Update index.php

--- hud-guide/index.php
+++ hud-guide/index.php
@@ -1,46 +1,14 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-<head>
-	<title>All About Huddersfield</title>
-	<meta http-equiv="content-type" content="text/html;charset=utf-8" >
-</head>
-<body>
-<header>
-<div> All About Huddersfield</div>
-<nav>
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="sport.php">Sport</a></li>
-        <li><a href="industry.php">Industry</a></li>
-    </ul>
-</nav>
-</header>
-
-=======
 <?php
 $pageTitle="Home page";
 include "header.php";
 ?>
->>>>>>> 78a33fe88f1b815f0f3e8c994ffc072056c5ba2b
 <section>
 <h1>Huddersfield</h1>
 <p>
 Huddersfield is a large market town and is the largest settlement in the metropolitan borough of Kirklees, West Yorkshire, England. It is the 11th largest town in the United Kingdom with a population of 162,949 (2011 census).Halfway between Leeds and Manchester, it lies 190 miles (310 km) north of London, and 10.3 miles (16.6 km) south of Bradford, the nearest city.</p>
 <p>Huddersfield is a town known for sport, home to the rugby league team, Huddersfield Giants, founded in 1895, who play in the European Super League and Football League Championship football team Huddersfield Town F.C., founded in 1908. The town is home to the University of Huddersfield and the sixth form colleges Greenhead College, Kirklees College and Huddersfield New College.</p>
 <section>
-<<<<<<< HEAD
-
-<footer>
-Text modified from
-<a href="https://en.wikipedia.org/wiki/Huddersfield">
-https://en.wikipedia.org/wiki/Huddersfield
-</a>
-</footer>
-</body>
-</html>
-=======
 <?php
 include "footer.php";
 ?>
->>>>>>> 78a33fe88f1b815f0f3e8c994ffc072056c5ba2b
+
