27c56d3630829dda68dfa0ba5112e38d26e0571b
mmantle-hud <m.e.mantle@hud.ac.uk>
2021-11-03 14:17:13 +0000

    Update sport.php

--- hud-guide/sport.php
+++ hud-guide/sport.php
@@ -1,46 +1,16 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-<head>
-	<title>All About Huddersfield</title>
-	<meta http-equiv="content-type" content="text/html;charset=utf-8">
-</head>
-<body>
-<header>
-<div> All About Huddersfield</div>
-<nav>
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="sport.php">Sport</a></li>
-        <li><a href="industry.php">Industry</a></li>
-    </ul>
-</nav>
-</header>
-
-=======
 <?php
 $pageTitle="Sport";
 include "header.php";
 ?>
->>>>>>> 78a33fe88f1b815f0f3e8c994ffc072056c5ba2b
+
 <section>
 <h1>Sport in Huddersfield</h1>
 <p>Football and rugby league are the main spectator sports in Huddersfield.
  Its professional football team, Huddersfield Town F.C. play in the Championship. In 1926, the club became the first in England to win three successive league titles, a feat only three other clubs have matched.The town was the birthplace of rugby league, and is home to the Huddersfield Giants who play in the Super League, the top division in Europe. The town is home to Huddersfield Underbank Rangers rugby league club, who play in the Rugby League Conference. Huddersfield Rugby Union Football Club play in the National Division Three North and Huddersfield Rams Aussie Rules club. The main sporting arena is John Smith's Stadium home to both the football team and rugby league side.
  </p>
 <section>
-<<<<<<< HEAD
 
-<footer>
-Text modified from
-<a href="https://en.wikipedia.org/wiki/Huddersfield">
-https://en.wikipedia.org/wiki/Huddersfield
-</a>
-</footer>
-</body>
-</html>
-=======
 <?php
 include "footer.php";
 ?>
->>>>>>> 78a33fe88f1b815f0f3e8c994ffc072056c5ba2b
+
